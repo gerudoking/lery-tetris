1ad81e43a9d6cd2b1e57e01f7cf8e2f09084d5ec
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-03-31 19:37:14 -0300

    Update Tela.h
    Adicionou a struct tela e a interface das funções.

--- Tela.h
+++ Tela.h
@@ -1,1 +1,16 @@
+#include <ncurses.h>
 
+typedef struct tipomatriz{
+  int ocupado = 0;
+};
+typedef struct tipomatriz Tipomatriz;
+
+typedef struct tela{
+  Tipomatriz matriz_interface[15][25];
+  char matriz_gui[30][30];
+};
+typedef struct tela Tela;
+
+Tela* cria_tela();
+
+int mostra_tela(Tela* t);//Mudei o retorno de void para int para poder fazer os testes
