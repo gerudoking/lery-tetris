272a476051c46a00a725550508cea35e46c5bbf1
BurningBagel <luiz.duarte11@yahoo.com.br>
2016-04-04 03:21:09 -0300

    Update Pecas.c

--- Pecas.c
+++ Pecas.c
@@ -2,15 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <ncurses.h>
+#include "Pecas.h"
 #include "Tela.h"
 #include "Tela.c"
-typedef struct{
-int posicao_x, posicao_y,tamanho,orientacao,cor;
-char* simbolo;
-
-}peca;
-typedef struct peca peca;
-
 peca* nova_peca(Tela* tela){
 	int i;
 	peca* a;
