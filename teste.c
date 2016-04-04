f980697296c250dbf13a789079872aeb3fecc4ba
0ZeroZeus <0ZeroZeus@gmail.com>
2016-04-04 19:21:17 -0300

    codigo para os testes

---
+++ teste.c
@@ -0,0 +1,10 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "CUnit/CUnit.h"
+#include "CUnit/Basic.h" 
+#include "Tela.c"
+#include "Pecas.c"
+#include "Engine.c"
