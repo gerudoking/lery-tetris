cf4b988ada1acda1e75e6c3367b300237c73cf80
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-04-05 18:48:58 -0300

    Altos updates. Ler a descrição extendida.
    *Agora inicializa as bordas como ocupadas.
    *Agora desenha um X nos lugares ocupados, exceto nas bordas(que são marcadas como ocupadas)

--- Tela.c
+++ Tela.c
@@ -1,13 +1,12 @@
 #include "Tela.h"
-#include <stdlib.h>
 
 Tela* cria_tela(){
 	Tela* t;
 	int i, j;
 
-	init_pair(1, COLOR_RED, COLOR_BLACK);
-	init_pair(2, COLOR_BLUE, COLOR_BLACK);
-	init_pair(3, COLOR_GREEN, COLOR_BLACK);
+	init_pair(1, COLOR_RED, COLOR_BLACK);//Trataremos o 1 como a cor vermelha nos caracteres
+	init_pair(2, COLOR_BLUE, COLOR_BLACK);//E o 2 como azul
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);//por sua vez o 3 como verde
 
 	t = (Tela*) malloc(sizeof(Tela));
 	
@@ -19,12 +18,18 @@ Tela* cria_tela(){
 
 	for(i = 0; i<25; i++){
 		(*t).matriz_gui[0][i].caracter = '=';
+		(*t).matriz_gui[0][i].ocupado = 1;
+		
 		(*t).matriz_gui[14][i].caracter = '=';
+		(*t).matriz_gui[14][i].ocupado = 1;
 	}
 
 	for(j = 0; j<15; j++){
 		(*t).matriz_gui[j][0].caracter = '|';
+		(*t).matriz_gui[j][0].ocupado = 1;
+		
 		(*t).matriz_gui[j][24].caracter = '|';
+		(*t).matriz_gui[j][24].ocupado = 1;
 	}
 
 	return t;
@@ -38,11 +43,11 @@ int mostra_tela(Tela* t){
 	for(i = 0; i<15; i++){
 		for(j = 0; j<25; j++){
 			attrset(COLOR_PAIR((*t).matriz_gui[i][j].pardecor));
-			mvaddch(i, j, (*t).matriz_gui[i][j].caracter);
 			
-			if((*t).matriz_gui[i][j].ocupado == 1){
-				mvaddch(i, j, 'X');
+			if(((*t).matriz_gui[i][i].ocupado) && ((*t).matriz_gui[i][i].caracter != '|') && ((*t).matriz_gui[i][i].caracter != '=')){
+				(*t).matriz_gui[i][i].caracter = 'X';
 			}
+			mvaddch(i, j, (*t).matriz_gui[i][j].caracter);
 		}
 	}
 	return 0;
