5d58f7b87bde1240c075b69220740a1a0ef4c4a3
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-04-08 18:38:48 -0300

    Adicionado a interface das telas finais e iniciais

--- Tela.h
+++ Tela.h
@@ -16,3 +16,7 @@ typedef struct tela Tela;
 Tela* cria_tela();
 
 int mostra_tela(Tela* t);//Mudei o retorno de void para int para poder fazer os testes
+
+int mostra_tela_inicial(Tela* t);
+
+int mostra_tela_final(Tela* t, int pont);
