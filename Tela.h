5e3f9b394cea9700dcf089cf45167ec239fbe13d
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-04-12 19:50:02 -0300

    Adicionada a interface das funções de janela

--- Tela.h
+++ Tela.h
@@ -13,6 +13,10 @@ Tipomatriz matriz_gui[15][25];
 };
 typedef struct tela Tela;
 
+WINDOW* cria_nova_janela(int alt, int larg, int starty, int startx);
+
+void destroi_janela(WINDOW* janela_local);
+
 Tela* cria_tela();
 
 int mostra_tela(Tela* t);//Mudei o retorno de void para int para poder fazer os testes
