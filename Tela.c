05523f8b2e418a1684ced5e4f7b3e0f610dacbc6
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-04-12 19:48:46 -0300

    Bordas agora desenhadas corretas. E mais.
    *A matriz_gui não possui bordas mais. É livre para o movimento de peças.
    *A borda é desenhada externamente como uma box. Pretty aint it?

--- Tela.c
+++ Tela.c
@@ -1,5 +1,22 @@
 #include "Tela.h"
 
+WINDOW* cria_nova_janela(int alt, int larg, int starty, int startx){
+	WINDOW* janela_local;
+
+	janela_local = newwin(alt, larg, starty, startx);
+	box(janela_local, 0 , 0);
+	
+	wrefresh(janela_local);
+
+	return janela_local;
+}
+
+void destroi_janela(WINDOW* janela_local){//Não sei se será necessária. É usada pra apagar as bordinhas.
+	wborder(janela_local, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+	wrefresh(janela_local);
+	delwin(janela_local);
+}
+
 Tela* cria_tela(void){
 	Tela* t;
 	int i, j;
@@ -15,34 +32,20 @@ Tela* cria_tela(void){
 		}
 	}
 
-	for(i = 0; i<25; i++){
-		(*t).matriz_gui[0][i].caracter = '=';
-		(*t).matriz_gui[0][i].ocupado = 1;
-		
-		(*t).matriz_gui[14][i].caracter = '=';
-		(*t).matriz_gui[14][i].ocupado = 1;
-	}
-
-	for(j = 0; j<15; j++){
-		(*t).matriz_gui[j][0].caracter = '|';
-		(*t).matriz_gui[j][0].ocupado = 1;
-		
-		(*t).matriz_gui[j][24].caracter = '|';
-		(*t).matriz_gui[j][24].ocupado = 1;
-	}
-
 	return t;
 }
 
 int mostra_tela(Tela* t){
 	int i, j;
-	
-	clear();
+	WINDOW* janela;
+
+	refresh();
+	janela = cria_nova_janela(18,27,0,0);
 
 	for(i = 0; i<15; i++){
 		for(j = 0; j<25; j++){
 			attrset(COLOR_PAIR((*t).matriz_gui[i][j].pardecor));
-			mvaddch(i, j, (*t).matriz_gui[i][j].caracter);
+			mvaddch(i+1, j+1, (*t).matriz_gui[i][j].caracter);
 		}
 	}
 	return 0;
@@ -57,8 +60,6 @@ int mostra_tela_inicial(void){
 	attrset(COLOR_PAIR(2));
 	mvprintw(10, 3, "Pressione qualquer");
 	mvprintw(11, 3, "tecla para começar");
-	attrset(COLOR_PAIR(1));
-	mvprintw(11, 22, "   |");
 
 	free(t);
 	return 0;
