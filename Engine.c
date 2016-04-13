260441a39fb67289a190fba33907444a770d5cc6
0ZeroZeus <0ZeroZeus@gmail.com>
2016-04-13 16:55:56 -0300

    Update Engine.c
    Consertadas as coordenadas do fixa_peca e a varivavel nao utilizada

--- Engine.c
+++ Engine.c
@@ -1,64 +1,69 @@
 #include <stdio.h>
+#include <ncurses.h>
+#include "Tela.c"
+#include "Pecas.c"
 #include "Engine.h"
-#include "Pecas.h"
-
 void inicia_ncurses(){
 	initscr();
-	cbreak();
-  noecho();
-  timeout(0);
+	keypad(stdscr, TRUE);
+ 	start_color();
+  	init_pair(1, COLOR_RED, COLOR_BLACK);//Trataremos o 1 como a cor vermelha nos caracteres
+	init_pair(2, COLOR_BLUE, COLOR_BLACK);//E o 2 como azul
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);//por sua vez o 3 como verde
 }
 
 void finaliza_ncurses(){
 	endwin();
 }
 
-int fixa_peca(Tela* tela, peca* tetromino){
-	int x, y;
+void fixa_peca(Tela* tela, peca* tetromino){
+	int x, y, i;
   
   x = tetromino->posicao_x;
   y = tetromino->posicao_y;
   
-  for(int i=0; i<*peca->tamanho; i++){
+  for(i=0; i<tetromino->tamanho; i++){
     if(tetromino->orientacao == 0){
-			tela->matriz_gui[x][y+i]->ocupado = 1;
-    	tela->matriz_gui[x][y+i]->caracter = 'X';  
-		}
+		tela->matriz_gui[y][x+i].ocupado = 1;
+    	tela->matriz_gui[y][x+i].caracter = 'X';  
+	}
     if(tetromino->orientacao == 1){
-			tela->matriz_gui[x+i][y]->ocupado = 1;
-    	tela->matriz_gui[x+i][y]->caracter = 'X';  
+		tela->matriz_gui[y+i][x].ocupado = 1;
+    	tela->matriz_gui[y+i][x].caracter = 'X';  
   	}
   }
-  return 1;
+  free(tetromino);
 }
 
-int deleta_linha(Tela* tela, int linha){
+void deleta_linha(Tela* tela, int linha){
   int i, cont, cont2;
   for (i=1; i<25; i++){
-    tela->matriz_gui[i][linha]->ocupado = 0;
-    tela->matriz_gui[i][linha]->caracter = ' ';
+    tela->matriz_gui[i][linha].ocupado = 0;
+    tela->matriz_gui[i][linha].caracter = ' ';
   }
   for(cont = linha; cont > 0; cont--){
     for(cont2=1; cont2 < 25; cont2++){
-      tela->matriz_gui[cont2][cont]->caracter = tela->matriz_gui[cont2-1][cont]->caracter; 
-      tela->matriz_gui[cont2][cont]->ocupado = tela->matriz_gui[cont2-1][cont]->ocupado; 
+      tela->matriz_gui[cont2][cont].caracter = tela->matriz_gui[cont2-1][cont].caracter; 
+      tela->matriz_gui[cont2][cont].ocupado = tela->matriz_gui[cont2-1][cont].ocupado; 
     }  
   }
 }
 
 int movimento(Tela* tela, int* pontuacao){
   
-	int locked, resultado, retorno, cont, cont2, fechou=0;
+	int locked, resultado, cont, cont2, fechou=0;
 	peca* tetromino = nova_peca(tela);
+
   	mostra_tela(tela);
+
 	locked = 0;
-	while(locked = 0) {
+	while(locked == 0) {
 		switch(getch()) {
 			case KEY_LEFT:
-				retorno = move_peca_x(tela, tetromino, -1);
+				move_peca_x(tela, tetromino, -1);
 				break;
 			case KEY_RIGHT:
-				retorno = move_peca_x(tela, tetromino, 1);
+				move_peca_x(tela, tetromino, 1);
 				break;
 			case KEY_DOWN:
 				resultado = move_peca_y(tela, tetromino);
@@ -68,6 +73,7 @@ int movimento(Tela* tela, int* pontuacao){
       			break;
 		}
     mostra_tela(tela);
+    refresh();
  
     if (resultado < 5){
       fixa_peca(tela, tetromino);
@@ -78,7 +84,7 @@ int movimento(Tela* tela, int* pontuacao){
   for(cont = 1; cont < 15; cont++){
     fechou = 0;
     for(cont2=1; cont2 < 25; cont2++){
-    	if(tela->matriz_gui[cont2][cont]->caracter == 'X')
+    	if(tela->matriz_gui[cont2][cont].caracter == 'X')
       	fechou++;
   	}
     if(fechou == 23){
@@ -88,19 +94,4 @@ int movimento(Tela* tela, int* pontuacao){
   }
     
   return 0;
-}
-
-int jogo() {
-	int end = 0;
-	int pontuacao = 0;
-  
-	Tela* tela = cria_tela();
-  
-	while(end == 0){
-  	
-  	mostra_tela(tela);
-		end = movimento(tela, &pontuacao);
-	}
-  
-  return pontuacao*100;
 }
