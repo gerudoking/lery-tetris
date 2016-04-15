10a7e0a7f54a346816ccad913e7fd48cae550ebe
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-04-15 10:40:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,33 @@
 #include "Pecas.h"
 #include "Engine.h"
 
-int main(
+int main(){
 
+	srand(time(NULL));
 
+	int end = 0;
+	int pontuacao = 0;
+	
+	inicia_ncurses();
 
+	Tela* t = cria_tela();
 
+	mostra_tela_inicial();
+	getch();
+  
+	while(end == 0){
+  	
+  		mostra_tela(t);
+  		//getch();
+		end = movimento(t, &pontuacao);
+	}
 
-return 0;
+	mostra_tela_final(pontuacao*100);
+	getch();
+
+	free(t);
+	finaliza_ncurses();
+
+	return 0;
 }
+
