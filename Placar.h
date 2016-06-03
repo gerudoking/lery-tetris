6270d34faafed5b69fd893801e386637812f86a5
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-06-03 18:19:58 -0300

    Declarado Jogador e a interface de funções

---
+++ Placar.h
@@ -0,0 +1,11 @@
+struct tipojogador{
+  char nome[6];
+  int pontos;
+};
+typedef struct tipojogador Jogador;
+
+void cria_placar(void);
+
+void atualiza_placar(int pontuacao);
+
+void mostra_placar(void);
