681a37b00fb8475d577a6fa0ee4e99c06a2fbca6
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-06-04 11:51:27 -0300

    Update Ordenacao.c

--- Ordenacao.c
+++ Ordenacao.c
@@ -1,1 +1,25 @@
+#include "Ordenacao.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+void ordena(FILE* arq){
+  Jogador *vetor;
+  int totalRegistros = 0;
+  int i;
+  
+  while(!feof(arq)){
+    fseek(arq, sizeof(char)*12, SEEK_CUR);
+    totalRegistros++;
+  }
+  
+  fseek(arq, 0, SEEK_SET);
+  
+  vetor = (Jogador*) malloc(sizeof(Jogador)*totalRegistros);
+  
+  for(i = 0; i < totalRegistros; i++){
+    fscanf(arq, "%s", vetor[i].nome);
+    fscanf(arq, "%d", &(vetor[i].pontos));
+  }
+  
+  quicksort(vetor, 0, totalRegistros - 1);
+}
