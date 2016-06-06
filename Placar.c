d96b831ab646a3835761ea6d3b9d47e0edb91add
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-06-06 13:43:56 -0300

    Update Placar.c

--- Placar.c
+++ Placar.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include "Placar.h"
-#include "Ordenacao.h"
 
 void cria_placar(void){
   FILE* arq;
@@ -17,26 +16,46 @@ void cria_placar(void){
 void atualiza_placar(int pontuacao){
   FILE* arq;
   Jogador j;
+  int i, checador;
   
   j.pontos = pontuacao;
   mvprintw(1, 1, "Digite o seu nome");
   mvprintw(2, 1, "em ate 5 letras:");
   mvscanw(3, 1, "%s", j.nome);
   
-  arq = fopen("placar.txt", "a");
+  arq = fopen("placar.txt", "r+");
   
-  fprintf(arq, "%s ", j.nome);
+  for(i = 0; i < MAX_PLACAR; i++){
+    fseek(arq, sizeof(char)*5, SEEK_CUR);
+    fscanf(arq, "%d", &checador);
+    if(j.pontos > checador){
+      fseek(arq, (sizeof(char))*(-11), SEEK_CUR);
+      
+      fprintf(arq, "%s ", j.nome);
   
-  //As seguintes checagens garantem que o campo pontos tenha tamanho fixo;
-  if(j.pontos < 100) fprintf(arq, "  ");
-  if(j.pontos < 1000) fprintf(arq, " ");
-  if(j.pontos < 10000) fprintf(arq, " ");
-  fprintf(arq, "%d", j.pontos);
+      //As seguintes checagens garantem que o campo pontos tenha tamanho fixo;
+      if(j.pontos < 100) fprintf(arq, "  ");
+      if(j.pontos < 1000) fprintf(arq, " ");
+      if(j.pontos < 10000) fprintf(arq, " ");
+      fprintf(arq, "%d", j.pontos);
+      
+      break;
+    }
+  }
   
   fclose(arq);
-  arq = fopen("placar.txt", "w+");
+}
+
+void mostra_placar(void){
+  FILE* arq;
+  int i;
+  char vetor[13];
   
-  //ordena(arq);
+  arq = fopen("placar.txt", "r");
+  
+  for(i = 0; i < MAX_PLACAR; i++){
+    
+  }
   
   fclose(arq);
 }
