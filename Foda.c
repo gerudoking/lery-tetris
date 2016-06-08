53fc7722e80780af3da58067d6d1eb886e0f5191
0ZeroZeus <0ZeroZeus@gmail.com>
2016-06-07 22:26:49 -0300

    Update Foda.c

--- Foda.c
+++ Foda.c
@@ -3,26 +3,7 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <poll.h>
-
-struct tipomatriz{
-	int ocupado;
-	int pardecor;
-	char caracter;
-};
-typedef struct tipomatriz Tipomatriz;
-
-struct tela{
-	Tipomatriz matriz_gui[15][25];
-};
-typedef struct tela Tela;
-
-typedef struct peca{
-	int posicao_x; 
-	int posicao_y;
-	int tamanho;
-	int orientacao;
-	int cor;
-}peca;
+#include "Pecas.h"
 
 WINDOW* cria_nova_janela(int alt, int larg, int starty, int startx){
 	WINDOW* janela_local;
@@ -104,140 +85,141 @@ int mostra_tela_final(int pont){
 peca* nova_peca(Tela* tela){
 	int i;
 	peca* a;
-	if((*tela).matriz_gui[1][13].caracter=='X') return NULL;
+	if((*tela).matriz_gui[1][13].ocupado==1) return NULL;
 	a=(peca*)malloc(sizeof(peca));
 	(*a).tamanho=((rand() %3)+3);/*Escolhe um tamanho entre 3 e 5 para a peca*/
-	(*a).orientacao=rand() %2;/*Define a orientação: 0 e vertical, 1 e horizontal*/
-	(*a).cor=rand() %3;
-	(*a).posicao_x=13;
-	(*a).posicao_y=1;
-		if((*a).orientacao==0){
+	(*a).tipo=rand() %6;/*Define o tipo de peca: 0 e vertical, 1 e horizontal, 2 e Z, 3 e quadrado, 4 e L, 5 e T*/
+	(*a).cor=rand() %3 + 1;
+	(*a).posicao_x=10;
+	(*a).posicao_y=4;
+	switch((*a).tipo){
+		case 0:
 			for(i=0;i<(*a).tamanho;i++){
-				(*tela).matriz_gui[1+i][13].caracter='X';
+				(*a).formato[i][3].caracter='X';
 			}
-		}
-		else{
+		break;
+		
+		case 1:
 			for(i=0;i<(*a).tamanho;i++){
-				(*tela).matriz_gui[1][13+i].caracter='X';
+				(*a).formato[3][i].caracter='X';
 			}
+		break;
+
+		case 2:/*peca Z*/
+			for(i=0;i<3;i++){
+				(*a).formato[2+i][3]='X';
+				if(i<2){
+					(*a).formato[2][i+2]='X';
+					(*a).formato[4][i+3]='X';
+				}
+			}
+		break;
+		
+		case 3:/*quadrado*/
+			for(i=0;i<2;i++){
+				for(j=0;j<2;j++){
+					(*a).formato[j+2][i+2]='X';
+				}
+			}
+		break;
+		
+		case 4:/*peca L*/
+			for(i=0;i<3;i++){
+				(*a).formato[2+i][2]='X';
+				(*a).formato[4][i+2]='X';
+			}
+		break;
+
+		case 5:/*peca T*/
+			for(i=0;i<5;i++){
+				(*a).formato[2][i+1]='X';
+				if(i<2) (*a).formato[i+3][3]='X';
+			}
+		break;
+	}
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			if((*a).formato[i][j]!='X') (*a).formato[i][j]=' ';
 		}
+	}
 	return a;
 }
 
 int move_peca_x(Tela* tela,peca* a,int direcao){/*vai receber um inteiro, que vai ser ou -1 ou 1, para mostrar se e para mover para a esquerda ou direita, respectivamente*/
-	int i,flag=0,x=(*a).posicao_x,y=(*a).posicao_y,Tamanho=(*a).tamanho;
-		if((*a).orientacao==0){/*dependendo da orientacao da peca(vertical ou horizontal), agiremos diferentemente*/
-			/*primeiro verificamos se a peca ja 								esta nos extremos da tela. Nesse caso, ela
-								nao pode mover*/
-			if(direcao==1){/*dependendo da direcao desejada do movimento, checaremos se ha parte de outra peca do lado adequado*/
-				if(x==23) return 1;
-				for(i=0;i<Tamanho;i++){
-					if((*tela).matriz_gui[y+i][x+1].ocupado==1){/*aqui checamos se ha parte de outra peca ao lado da peca ativa. a flag sera usada para sair da funcao caso isso seja verdade*/
-						flag=1;
-						i=Tamanho;
-					}
-				}
-				if(flag!=0) return 2;/*cada possivel caso de erro tem o seu retorno, permitindo
-						acoes de acordo com cada caso*/
-				for(i=0;i<Tamanho;i++){
-					(*tela).matriz_gui[y+i][x].caracter=' ';/*Temos que limpar o lugar que a peca ocupava antes de move-la*/
-				}
-				((*a).posicao_x)++;/*finalmente, a posicao na struct da peca e mudada*/			x=((*a).posicao_x);
-				for(i=0;i<Tamanho;i++){
-					(*tela).matriz_gui[y+i][x].caracter='X';
-				}
-				return 5;
-			}
-
-			if(direcao==-1){
-				if(x==1) return 1;
-				for(i=0;i<Tamanho;i++){
-					if((*tela).matriz_gui[y+i][x-1].ocupado==1){
-						flag=1;
-						i=Tamanho;
-					}
-				}
-				if(flag!=0) return 3;
-				for(i=0;i<Tamanho;i++){
-					(*tela).matriz_gui[y+i][x].caracter=' ';
-				}
-				((*a).posicao_x)--;/*finalmente, a posicao na struct da peca e mudada*/			x=((*a).posicao_x);
-				for(i=0;i<Tamanho;i++){
-					(*tela).matriz_gui[y+i][x].caracter='X';
-				}
-				return 5;
-			}
-		return 3;/*nesse ponto, se a funcao nao terminou, alguma coisa deu errado*/
+	int i,j;,
+	int x=(*a).posicao_x;
+	int y=(*a).posicao_y;
+	int Tamanho=(*a).tamanho;
+	int cor=(*a).cor;
+	x+=direcao;
+	for(i=0;i<7;i++){
+		for(j=0;j<7;j++){
+			if((*a).formato[i][j]=='X' && (*tela).matriz_gui[y+i][x+j].ocupado==1) return 1;
+			else if((*a).formato[i][j]=='X') (*tela).matriz_gui[y+i][x+j].character=' ';
 		}
-		if((*a).orientacao==1){
-			if(direcao==1){
-				if(x+Tamanho==24) return 4;
-				if((*tela).matriz_gui[y][x+Tamanho].ocupado==1) return 5;/*com pecas horizontais, so precisamos checar 2 posicoes*/
-				for(i=0;i<Tamanho;i++){
-					(*tela).matriz_gui[y][x+i].caracter=' ';
-				}
-				((*a).posicao_x)++;
-				x=((*a).posicao_x);
-				for(i=0;i<Tamanho;i++){
-					(*tela).matriz_gui[y][x+i].caracter='X';
-				}
-				return 5;
+	}
+	(*a).posicao_x+=direcao;
+	for(i=0;i<7;i++){
+		for(j=0;j<7;j++){
+			if((*a).formato[i][j]=='X') {
+				tela.matriz_gui[y+i][x+j].character='X';
+				(*tela).matriz_gui[y+i][x+j].pardecor=cor;
 			}
-			if(direcao==-1){
-				if(x==1) return 4;
-				if((*tela).matriz_gui[y][x-1].ocupado==1) return 6;
-				for(i=0;i<Tamanho;i++){
-					(*tela).matriz_gui[y][x+i].caracter=' ';
-				}
-				((*a).posicao_x)--;
-				x=((*a).posicao_x);
-				for(i=0;i<Tamanho;i++){
-					(*tela).matriz_gui[y][x+i].caracter='X';
-				}
-				return 5;	
-			}
-			return 6;
 		}
-	return 8;
+	}
+	return 0;	
 }
 
+
 int move_peca_y(Tela* tela,peca* a){/*Como so podemos mover para baixo, nao necessitamos de entrada de inteiros*/
-	int x=(*a).posicao_x,y=(*a).posicao_y,Tamanho=(*a).tamanho,i,flag=0;
-	if((*a).orientacao==0){
-		if(y+Tamanho==15) return 1; /*Caso a peca ja esteja no fim da tela, ela nao pode mover*/
-		if((*tela).matriz_gui[y+Tamanho][x].ocupado==1) return 2; /*Caso exista parte de uma peca embaixo da peca, ela deve parar*/
-		for(i=0;i<Tamanho;i++){
-			(*tela).matriz_gui[y+i][x].caracter=' ';
-		}
-		((*a).posicao_y)+=1;
-		y=((*a).posicao_y);
-		for(i=0;i<Tamanho;i++){
-			(*tela).matriz_gui[y+i][x].caracter='X';
-		}
-	return 5;
-	}
+	int x=(*a).posicao_x;
+	int y=(*a).posicao_y;
+	int Tamanho=(*a).tamanho;
+	int i,j;
+	int cor= (*a).cor;
 	
-	if((*a).orientacao==1){
-		if(y==14) return 3;/*teste de encontro com borda da tela*/
-		
-		for(i=0;i<Tamanho;i++){
-			if((*tela).matriz_gui[y+1][x+i].ocupado==1){
-				flag=1;
-				i=Tamanho;
+	y++;
+	for(i=0;i<7;i++){
+		for(j=0;j<7;j++){
+			if((*a).formato[i][j]=='X' && (*tela).matriz_gui[y+i][x+j].ocupado==1) return 1;
+			else if((*a).formato[i][j]=='X') (*tela).matriz_gui[y+i][x+j].character=' ';
+		}
+	}
+	(*a).posicao_x+=direcao;
+	for(i=0;i<7;i++){
+		for(j=0;j<7;j++){
+			if((*a).formato[i][j]=='X') {
+				tela.matriz_gui[y+i][x+j].character='X';
+				(*tela).matriz_gui[y+i][x+j].pardecor=cor;
 			}
 		}
-		if(flag==1) return 4;
-		for(i=0;i<Tamanho;i++){
-			(*tela).matriz_gui[y][x+i].caracter=' ';
-		}
-		((*a).posicao_y)+=1;
-		y=((*a).posicao_y);
-		for(i=0;i<Tamanho;i++){
-			(*tela).matriz_gui[y][x+i].caracter='X';
-		}
-		return 5;
 	}
-	return 6;
+	return 0;
+}
+peca* rotaciona_peca(peca* a,Tela* tela){
+	int i,j;
+	char compara[7][7];
+	int x=(*a).posicao_x;
+	int y=(*a).posicao_y;
+	
+	for(i=0;i<7;i++){
+		for(j=0;j<7;j++){
+			compara[j][i]=(*a).formato[i][j];
+		}	
+	}
+	
+	for(i=0;i<7;i++){
+		for(j=0;j<7;j++){
+			if(compara[i][j]=='X' && (*tela).matriz_gui[y+i][x+j].ocupado==1) return 1;
+		}
+	}
+	
+	for(i=0;i<7;i++){
+		for(j=0;j<7;j++){
+			(*a).formato[i][j]=compara[i][j];
+		}
+	}
+	return 0;
 }
 
 void inicia_ncurses(){
