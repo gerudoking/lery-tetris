b1b291d60aebfb535d90b063968234fbbd5316df
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-06-11 13:29:16 -0300

    Update fodaP1.c

--- fodaP1.c
+++ fodaP1.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <poll.h>
 #include "PecasP1.h"
+#include "Placar.h"
 
 WINDOW* cria_nova_janela(int alt, int larg, int starty, int startx){
 	WINDOW* janela_local;
@@ -77,6 +78,20 @@ int mostra_tela_final(int pont){
 	attrset(COLOR_PAIR(2));
 	mvprintw(10, 4, "Sua pontuação é:");
 	mvprintw(11, 12, "%d", pont);
+
+	free(t);
+	return 0;
+}
+
+int mostra_tela_placar(int pont){
+	Tela* t = cria_tela();
+	mostra_tela(t);
+
+	cria_placar();
+	atualiza_placar(pont);
+	
+	mostra_tela(t);
+	mostra_placar();
 
 	free(t);
 	return 0;
@@ -340,6 +355,70 @@ int movimento(Tela* tela, int* pontuacao){
   return 0;
 }
 
+void cria_placar(void){
+  FILE* arq;
+  int i;
+  
+  arq = fopen("placar.txt", "r");
+  if (arq == NULL){
+    arq = fopen("placar.txt", "w");
+    
+    for(i = 0; i < 5; i++)
+    	fprintf(arq, "AAAAA     0\n");
+  }
+  
+  fclose(arq);
+}
+
+void atualiza_placar(int pontuacao){
+  FILE* arq;
+  Jogador j;
+  int i, checador;
+  
+  j.pontos = pontuacao;
+  mvprintw(1, 1, "Digite o seu nome");
+  mvprintw(2, 1, "em ate 5 letras:");
+  mvscanw(3, 1, "%s", j.nome);
+  
+  arq = fopen("placar.txt", "r+");
+  
+  for(i = 0; i < MAX_PLACAR; i++){
+    fseek(arq, sizeof(char)*5, SEEK_CUR);
+    fscanf(arq, "%d", &checador);
+    if(j.pontos > checador){
+      fseek(arq, (sizeof(char))*(-11), SEEK_CUR);
+      
+      fprintf(arq, "%s ", j.nome);
+  
+      //As seguintes checagens garantem que o campo pontos tenha tamanho fixo;
+      if(j.pontos < 100) fprintf(arq, "  ");
+      if(j.pontos < 1000) fprintf(arq, " ");
+      if(j.pontos < 10000) fprintf(arq, " ");
+      fprintf(arq, "%d", j.pontos);
+      
+      break;
+    }
+  }
+  
+  fclose(arq);
+}
+
+void mostra_placar(void){
+  FILE* arq;
+  int i;
+  char vetor[13];
+  
+  arq = fopen("placar.txt", "r");
+  
+  for(i = 0; i < MAX_PLACAR; i++){
+    fread(vetor, sizeof(char), 12, arq);
+		vetor[12] = '\0';
+		mvprintw(1*i, 2, "%s", vetor);
+  }
+  
+  fclose(arq);
+}
+
 int main(){
 
 	srand(time(NULL));
@@ -362,6 +441,9 @@ int main(){
 	}
 
 	mostra_tela_final(pontuacao*100);
+	getch();
+	
+	mostra_tela_placar(pontuacao*100);
 	getch();
 
 	free(t);
