dc9151bee20282ceb23c8c51019ecee43c5cd029
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-07-04 13:59:42 -0300

    Update Placar_Teste.c

--- Placar_Teste.c
+++ Placar_Teste.c
@@ -1,1 +1,10 @@
 
+#include "Placar.c"
+#include "CUnit/CUnit.h"
+#include "CUnit/Basic.h"
+
+void adicionar_suite(void);
+
+void teste_cria_placar(void);
+void teste_atualiza_placar(void);
+void teste_mostra_placar(void);
