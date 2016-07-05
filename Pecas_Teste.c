c7b94ca739e24ceac905ef210ac39a46cf558290
BurningBagel <luiz.duarte11@yahoo.com.br>
2016-07-04 21:55:56 -0300

    Update Pecas_Teste.c

--- Pecas_Teste.c
+++ Pecas_Teste.c
@@ -30,6 +30,13 @@ void teste_move_peca_y(){
 	free(a);
 	CU_ASSERT_EQUAL(resultado,5);
 }
+void teste_rotaciona_peca(){
+	Tela b;
+	peca* a=nova_peca(&b);
+	rotaciona_peca(&b,a);
+	CU_ASSERT_PTR_NOT_NULL(a);
+	free(a);
+}
 void adicionar_suite(){
 	CU_pSuite suite;
 	suite=CU_add_suite("Testes de Pecas.c",NULL,NULL);
@@ -37,6 +44,7 @@ void adicionar_suite(){
 	CU_ADD_TEST(suite, teste_move_peca_x_direita);
 	CU_ADD_TEST(suite, teste_move_peca_x_esquerda);
 	CU_ADD_TEST(suite, teste_move_peca_y);
+	CU_ADD_TEST(suite, teste_rotaciona_peca);
 }
 int main(){
 if (CUE_SUCCESS != CU_initialize_registry())
