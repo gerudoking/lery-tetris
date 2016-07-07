f81a8e43fe6b49f84a31c4716c7b01144074b5bf
BurningBagel <luiz.duarte11@yahoo.com.br>
2016-07-07 19:01:00 -0300

    Update Pecas_Teste.c

--- Pecas_Teste.c
+++ Pecas_Teste.c
@@ -1,18 +1,32 @@
 #include "Pecas.c"
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h"
+/*
+*	\author Luiz Felipe Carvalho Duarte
+*	\since 30/06/2016
+*	\version 1.0
+*/
+/*	Testamos primeiramente a criação de novas peças por caixa preta.
+*	Nos asseguramos de que a função de criação de novas peças não retorna null.
+*
+*/
 void teste_nova_peca(){
 	Tela* b;
 	peca* a=nova_peca(b);
 	CU_ASSERT_PTR_NOT_NULL(a);
+	free(a);
 }
+/*
+*	Aqui nos asseguramos de que a função de movimento horizontal funciona, por meio de verificação de seu retorno. São testados os movimentos para a esquerda e direita.
+*
+*/
 void teste_move_peca_x_direita(){
 	int resultado;	
 	Tela b;
 	peca* a=nova_peca(&b);
 	resultado=move_peca_x(&b,a,1);
 	free(a);
-	CU_ASSERT_EQUAL(resultado,5);
+	CU_ASSERT_EQUAL(resultado,0);
 }
 void teste_move_peca_x_esquerda(){
 	int resultado;	
@@ -20,22 +34,19 @@ void teste_move_peca_x_esquerda(){
 	peca* a=nova_peca(&b);
 	resultado=move_peca_x(&b,a,-1);
 	free(a);
-	CU_ASSERT_EQUAL(resultado,5);
+	CU_ASSERT_EQUAL(resultado,0);
 }
+/*
+*	Como com o movimento horizontal, nos asseguramos de que a função de movimento vertical retorna
+*	o valor que indica o funcionamento correto.
+*/
 void teste_move_peca_y(){
 	int resultado;	
 	Tela b;
 	peca* a=nova_peca(&b);
 	resultado=move_peca_y(&b,a);
 	free(a);
-	CU_ASSERT_EQUAL(resultado,5);
-}
-void teste_rotaciona_peca(){
-	Tela b;
-	peca* a=nova_peca(&b);
-	rotaciona_peca(&b,a);
-	CU_ASSERT_PTR_NOT_NULL(a);
-	free(a);
+	CU_ASSERT_EQUAL(resultado,8);
 }
 void adicionar_suite(){
 	CU_pSuite suite;
@@ -44,7 +55,6 @@ void adicionar_suite(){
 	CU_ADD_TEST(suite, teste_move_peca_x_direita);
 	CU_ADD_TEST(suite, teste_move_peca_x_esquerda);
 	CU_ADD_TEST(suite, teste_move_peca_y);
-	CU_ADD_TEST(suite, teste_rotaciona_peca);
 }
 int main(){
 if (CUE_SUCCESS != CU_initialize_registry())
