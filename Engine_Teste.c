14a162976aed6b7f01d760aa57a7cc2ecd4afcf1
0ZeroZeus <0ZeroZeus@gmail.com>
2016-07-07 19:00:00 -0300

    Update Engine_Teste.c

--- Engine_Teste.c
+++ Engine_Teste.c
@@ -4,45 +4,85 @@
 #include <string.h>
 
 #include "CUnit/CUnit.h"
-#include "CUnit/Basic.h" 
-#include "Tela.c"
-#include "Pecas.c"
+#include "CUnit/Basic.h"
 #include "Engine.c"
 
 
 void adicionar_suite(void);
-void fixa_peca(Tela* tela, peca* tetromino);
+void inicia_ncurses();
+int finaliza_ncurses();
 void deleta_linha(Tela* tela, int linha);
+int movimento(Tela* tela, int* pontuacao);
 
-void teste_fixa_peca(void){
-	int x, y;
-	Tela* tela = cria_tela();
-  	peca* tetromino = nova_peca(tela);
-  	x = tetromino->posicao_x;
-  	y = tetromino->posicao_y;
-  	fixa_peca(tela, tetromino);
 
-	CU_ASSERT_TRUE( (tela->matriz_gui[x][y].ocupado == 1) && (tela->matriz_gui[x][y].caracter == 'X') );
+void teste_inicia_ncurses(){
+	
+  	inicia_ncurses();
+  	CU_ASSERT(COLOR_PAIR(1));
+  	CU_ASSERT(COLOR_PAIR(2));
+  	CU_ASSERT(COLOR_PAIR(3));
 }
 
-void teste_deleta_linha(void){
+void teste_finaliza_ncurses(){
+  	CU_ASSERT_TRUE( finaliza_ncurses() == OK );
+}
+
+
+void teste_deleta_linha(){
 	int i, j;
 	Tela* tela = cria_tela();
-	for(i=6; i<25; i++)
-		for(j=1; j<15; j++)
+	for(i=1; i<25; i++)
+		for(j=7; j<15; j++)
 			tela->matriz_gui[i][j].ocupado = 1;
 
-	deleta_linha(tela, 13);
+	deleta_linha(tela, 10);
 
-	CU_ASSERT_TRUE( (tela->matriz_gui[6][13].ocupado == 0) && (tela->matriz_gui[7][13].ocupado == 1) );
+	CU_ASSERT_TRUE( (tela->matriz_gui[13][6].ocupado == 0) && (tela->matriz_gui[13][7].ocupado == 1) );
+  for(i = 0; i<15; i++){
+		for(j = 0; j<25; j++){
+        tela->matriz_gui[i][j].caracter = 'X';
+        tela->matriz_gui[i][j].ocupado = 1;
+      }
+  }
+  for(i = 0; i<15; i++)
+    deleta_linha(tela, i);
+  
+  CU_ASSERT_TRUE(tela->matriz_gui[8][16].caracter == 32 && tela->matriz_gui[10][21].ocupado == 0);
 }
+
+void teste_movimento(){
+	int i, j, end=0;
+	int pont = 0;
+	Tela* tela = cria_tela();
+
+	mostra_tela(tela);
+	movimento(tela, &pont);
+	
+	CU_ASSERT_TRUE(pont == 0);
+
+	for(i = 1; i<24; i++){
+		for(j = 12; j<15; j++){
+			tela->matriz_gui[j][i].caracter = 'X';
+			tela->matriz_gui[j][i].ocupado = 1;
+		}
+	}
+	mostra_tela(tela);
+	refresh();
+	
+	movimento(tela, &pont);
+	
+	CU_ASSERT_TRUE(pont != 0);
+}
+
 
 void adicionar_suite(){
 	CU_pSuite suite;
 	suite = CU_add_suite("lery_tetris",NULL,NULL);
 	
-	CU_ADD_TEST(suite, teste_fixa_peca);
+  	CU_ADD_TEST(suite, teste_inicia_ncurses);
 	CU_ADD_TEST(suite, teste_deleta_linha);
+	CU_ADD_TEST(suite, teste_movimento);
+  	CU_ADD_TEST(suite, teste_finaliza_ncurses);
 
 }
 
