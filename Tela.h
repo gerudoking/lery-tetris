1f4cc78f72dac6ac220caa820e85fa0e5d758184
Eduardo de Azevêdo dos Santos <edsantos2003@gmail.com>
2016-07-07 18:18:00 -0300

    Adicionado comentários para Doxygen

--- Tela.h
+++ Tela.h
@@ -18,12 +18,37 @@ WINDOW* cria_nova_janela(int alt, int larg, int starty, int startx);
 
 void destroi_janela(WINDOW* janela_local);
 
+/**
+ * Para esta função, foi utilizado o seguinte critério de teste:
+ * Caso a tela retornada pela função não seja nula, temos a
+ * função como bem sucedida.
+*/
 Tela* cria_tela();
 
+/**
+ * Para esta função, foi utilizado o seguinte critério de teste:
+ * Caso a função retorne 0, sua execução foi bem sucedida e rodou
+ * até o final. Caso contrário, houve algum erro.
+*/
 int mostra_tela(Tela* t);//Mudei o retorno de void para int para poder fazer os testes
 
+/**
+ * Para esta função, foi utilizado o seguinte critério de teste:
+ * Caso a função retorne 0, sua execução foi bem sucedida e rodou
+ * até o final. Caso contrário, houve algum erro.
+*/
 int mostra_tela_inicial();
 
+/**
+ * Para esta função, foi utilizado o seguinte critério de teste:
+ * Caso a função retorne 0, sua execução foi bem sucedida e rodou
+ * até o final. Caso contrário, houve algum erro.
+*/
 int mostra_tela_final(int pont);
 
+/**
+ * Para esta função, foi utilizado o seguinte critério de teste:
+ * Caso a função retorne 0, sua execução foi bem sucedida e rodou
+ * até o final. Caso contrário, houve algum erro.
+*/
 int mostra_tela_placar(int pont);
